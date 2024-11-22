c71ebdfd1595d028794bd71a0f3a8b61985feabe
puntoenv <sebssjb@gmail.com>
2024-11-22 12:39:18 -0500

    ADD: Data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <body>
         <div
           className={`relative flex flex-col min-h-screen ${raleway.className}`}
