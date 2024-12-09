3d2e6476e4abfda274201b4a9adb31a09dc87d09
puntoenv <sebssjb@gmail.com>
2024-12-09 09:22:19 -0500

    ADD: Data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,26 @@ const raleway = Raleway({
 export const metadata: Metadata = {
   title: "Colorantes Textiles Romveic",
   description: "Colorantes textiles de calidad para tus telas y prendas",
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL || "https://www.romevic.pe/"
+  ),
+  alternates: {
+    canonical: process.env.NEXT_PUBLIC_SITE_URL || "https://www.romevic.pe/",
+  },
+  openGraph: {
+    title: "Colorantes Textiles Romveic",
+    description: "Calidad y color para tus proyectos textiles.",
+    url: "https://www.romevic.pe",
+    type: "website",
+    images: [
+      {
+        url: "https://www.romevic.pe/icon.png",
+        width: 800,
+        height: 600,
+        alt: "Colorantes Textiles Romveic",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -21,7 +41,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>
         <div
           className={`relative flex flex-col min-h-screen ${raleway.className}`}
