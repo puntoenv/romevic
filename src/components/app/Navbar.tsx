335563e6260c04ec01d5f8a5b90abfe0570c754d
puntoenv <sebssjb@gmail.com>
2024-12-10 15:54:27 -0500

    *

--- src/components/app/Navbar.tsx
+++ src/components/app/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import clsx from "clsx";
-import { Building, Phone, ShoppingBag } from "lucide-react";
+import { Phone, ShoppingBag } from "lucide-react";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
